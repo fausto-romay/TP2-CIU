7a285cfb842569210b5ec45910a15caed2982ffb
fausto-romay <fromay972@gmail.com>
2025-11-05 22:09:57 -0300

    diseño del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,18 @@ export default function Footer() {
     return (
         <footer className="footer">
         <div className="footer-container">
-            <p className="footer-text">
-            ©2025 Todos los derechos reservados - Grupo 10 CIU/Unahur
-            </p>
+            <div className="footer-content">
+                <h2 className="footer-logo">Unahur Anti-Social Net</h2>
+                <ul className="footer-links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Explorar</a></li>
+                <li><a href="#">Soporte</a></li>
+                <li><a href="#">Contacto</a></li>
+                </ul>
+                <p className="footer-copy">
+                © 2025 Anti-Social — Grupo 10 CIU/Unahur
+                </p>
+            </div>
         </div>
         </footer>
     );
